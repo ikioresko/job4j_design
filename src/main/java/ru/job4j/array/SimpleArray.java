3a26cb198e885123ac209b2223d6696017c3b826
ikioresko <ikioresko@gmail.com>
2021-02-13 11:03:07 +0500

    5.2.1. Реализовать SimpleArray<T> [#467309]

--- src/main/java/ru/job4j/array/SimpleArray.java
+++ src/main/java/ru/job4j/array/SimpleArray.java
@@ -7,53 +7,34 @@ import java.util.Objects;
 
 public class SimpleArray<T> implements Iterable<T> {
     private Object[] tArray;
-    private final int length;
     private int index = 0;
     private int cursor = 0;
 
-    SimpleArray(int length) {
+    public SimpleArray(int length) {
         tArray = new Object[length];
-        this.length = length;
     }
 
-    void add(T model) {
-        try {
-            tArray[Objects.checkIndex(index, length)] = model;
-            index++;
-        } catch (IndexOutOfBoundsException ioe) {
-            System.out.println("Array is full");
-        }
+    public void add(T model) {
+        tArray[Objects.checkIndex(index++, index)] = model;
     }
 
-    void set(int index, T model) {
-        try {
-            tArray[Objects.checkIndex(index, length)] = model;
-        } catch (IndexOutOfBoundsException ioe) {
-            System.out.println("Index = " + index + " doesn't exist");
-        }
+    public void set(int index, T model) {
+        tArray[Objects.checkIndex(index, this.index)] = model;
     }
 
-    T get(int index) {
-        try {
-            return (T) tArray[Objects.checkIndex(index, length)];
-        } catch (IndexOutOfBoundsException ioe) {
-            System.out.println("Index = " + index + " doesn't exist");
-        }
-        return null;
+    public T get(int index) {
+        return (T) tArray[Objects.checkIndex(index, this.index)];
     }
 
     public void remove(int index) {
-        Object[] temp = tArray;
-        try {
+        if (index == Objects.checkIndex(index, this.index)) {
             System.arraycopy(tArray,
-                    Objects.checkIndex(index, length) + 1,
-                    temp,
+                    index + 1,
+                    tArray,
                     index,
                     tArray.length - (index + 1));
-            temp[temp.length - 1] = null;
-            tArray = temp;
-        } catch (IndexOutOfBoundsException ioe) {
-            System.out.println("Index = " + index + " doesn't exist");
+            tArray[tArray.length - 1] = null;
+            this.index--;
         }
     }
 
--- src/test/java/ru/job4j/array/SimpleArrayTest.java
+++ src/test/java/ru/job4j/array/SimpleArrayTest.java
@@ -3,21 +3,11 @@ package ru.job4j.array;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
-import org.junit.Before;
 import org.junit.Test;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 import java.util.NoSuchElementException;
 
 public class SimpleArrayTest {
-    private static final String LN = System.lineSeparator();
-    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-
-    @Before
-    public void setUp() {
-        System.setOut(new PrintStream(outContent));
-    }
 
     @Test
     public void testAddAndGet() {
@@ -30,7 +20,6 @@ public class SimpleArrayTest {
         assertThat(ss.get(1), is("Василий"));
         assertThat(ss.get(2), is("Иван"));
         assertThat(ss.get(3), is("Артем"));
-        assertNull(ss.get(4));
     }
 
     @Test
@@ -42,7 +31,6 @@ public class SimpleArrayTest {
         ss.remove(0);
         assertThat(ss.get(0), is(20));
         assertThat(ss.get(1), is(30));
-        assertNull(ss.get(2));
     }
 
     @Test
@@ -51,45 +39,41 @@ public class SimpleArrayTest {
         ss.add(10);
         ss.add(20);
         ss.add(30);
-        ss.set(1, 99);
+        ss.set(2, 99);
         assertThat(ss.get(0), is(10));
-        assertThat(ss.get(1), is(99));
-        assertThat(ss.get(2), is(30));
+        assertThat(ss.get(1), is(20));
+        assertThat(ss.get(2), is(99));
     }
 
-    @Test
+    @Test(expected = IndexOutOfBoundsException.class)
     public void addWhenArrayIsFull() {
         SimpleArray<Integer> ss = new SimpleArray<>(1);
         ss.add(10);
         ss.add(20);
-        assertEquals("Array is full" + LN, outContent.toString());
     }
 
-    @Test
+    @Test(expected = IndexOutOfBoundsException.class)
     public void whenInvalidIndexOfSetMethod() {
         SimpleArray<Integer> ss = new SimpleArray<>(2);
         ss.add(10);
         ss.add(20);
         ss.set(3, 99);
-        assertEquals("Index = 3 doesn't exist" + LN, outContent.toString());
     }
 
-    @Test
+    @Test(expected = IndexOutOfBoundsException.class)
     public void whenInvalidIndexOfGetMethod() {
         SimpleArray<Integer> ss = new SimpleArray<>(2);
         ss.add(10);
         ss.add(20);
         ss.get(1001);
-        assertEquals("Index = 1001 doesn't exist" + LN, outContent.toString());
     }
 
-    @Test
+    @Test(expected = IndexOutOfBoundsException.class)
     public void whenInvalidIndexOfRemoveMethod() {
         SimpleArray<Integer> ss = new SimpleArray<>(2);
         ss.add(10);
         ss.add(20);
         ss.remove(4);
-        assertEquals("Index = 4 doesn't exist" + LN, outContent.toString());
     }
 
     @Test
