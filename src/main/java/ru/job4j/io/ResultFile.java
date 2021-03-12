dfba13a95d748efc95b5ba064229672c20c9ff5d
ikioresko <ikioresko@gmail.com>
2021-03-12 17:51:53 +0500

    0.1. FileOutputStream. [#467336]

---
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -0,0 +1,24 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class ResultFile {
+    public static String[] table(int num) {
+        String[] str = new String[11];
+        for (int i = 0; i < 11; i++) {
+            str[i] = num + " * " + i + " = " + i * num;
+        }
+        return str;
+    }
+
+    public static void main(String[] args) {
+        try (FileOutputStream out = new FileOutputStream("result.txt")) {
+            for (String str : table(5)) {
+                out.write(str.getBytes());
+                out.write("\n".getBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
