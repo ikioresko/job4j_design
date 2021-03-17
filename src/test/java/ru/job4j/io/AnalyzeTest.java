9145d09f0765563c715a6f1616212d0a6bdc5bf3
ikioresko <ikioresko@gmail.com>
2021-03-17 12:41:34 +0500

    3.0. Тестирование IO [#467334]

--- src/test/java/ru/job4j/io/AnalyzeTest.java
+++ src/test/java/ru/job4j/io/AnalyzeTest.java
@@ -20,19 +20,19 @@ public class AnalyzeTest {
         File source = folder.newFile("source.txt");
         File target = folder.newFile("target.txt");
         try (PrintWriter out = new PrintWriter(source)) {
-            out.println("200 10:56:01" + ls
-                    + "500 10:57:01" + ls
-                    + "400 10:58:01" + ls
-                    + "200 10:59:01" + ls
-                    + "500 11:01:02" + ls
-                    + "200 11:02:02");
+            out.println("200 10:56:01");
+            out.println("500 10:57:01");
+            out.println("400 10:58:01");
+            out.println("200 10:59:01");
+            out.println("500 11:01:02");
+            out.println("200 11:02:02");
         }
         analyze.unavailable(source, target);
         StringBuilder rsl = new StringBuilder();
         try (BufferedReader in = new BufferedReader(new FileReader(target))) {
             in.lines().forEach(r -> rsl.append(r).append(ls));
-                    assertThat(rsl.toString(), is("10:57:01;10:59:01" + ls
-                + "11:01:02;11:02:02" + ls));
+            assertThat(rsl.toString(), is("10:57:01;10:59:01" + ls
+                    + "11:01:02;11:02:02" + ls));
         }
     }
 }
