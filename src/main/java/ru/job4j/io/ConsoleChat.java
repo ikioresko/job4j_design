796f14b6012d0ec8839593f5cdf33b898b734d5e
ikioresko <ikioresko@gmail.com>
2021-03-18 14:27:10 +0500

    6. Кодировка. [#467345]

---
+++ src/main/java/ru/job4j/io/ConsoleChat.java
@@ -0,0 +1,68 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.util.*;
+
+public class ConsoleChat {
+    private static final String OUT = "закончить";
+    private static final String STOP = "стоп";
+    private static final String CONTINUE = "продолжить";
+    private final String path;
+    private final String botAnswers;
+    private final List<String> botAnswerList;
+
+    public ConsoleChat(String path, String botAnswers) {
+        this.path = path;
+        this.botAnswers = botAnswers;
+        botAnswerList = new ArrayList<>();
+    }
+
+    public void run() {
+        String in = "";
+        boolean pause = false;
+        List<String> list = new ArrayList<>();
+        try (PrintWriter writer = new PrintWriter(new BufferedOutputStream(
+                new FileOutputStream(path)))) {
+            while (!in.equals(OUT)) {
+                Scanner input = new Scanner(System.in);
+                System.out.println("Задайте вопрос или напишите что-нибудь: ");
+                in = input.nextLine();
+                list.add(in);
+                if (in.equals(STOP)) {
+                    pause = true;
+                } else if (in.equals(CONTINUE)) {
+                    pause = false;
+                }
+                if (!pause && !in.equals(OUT)) {
+                    System.out.println("Ответ: " + answer());
+                }
+            }
+            writer.print(list);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String answer() {
+        if (botAnswerList.size() == 0) {
+            String line;
+            try (BufferedReader reader = new BufferedReader(new FileReader(botAnswers))) {
+                while ((line = reader.readLine()) != null) {
+                    botAnswerList.add(line);
+                }
+                if (botAnswerList.size() <= 1) {
+                    throw new IndexOutOfBoundsException(
+                            "Bot answer list is empty or have only one answer");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return botAnswerList.get((int) (Math.random() * botAnswerList.size()));
+    }
+
+    public static void main(String[] args) {
+        ConsoleChat cc = new ConsoleChat("logs.txt", "botAnswers.txt");
+        cc.run();
+    }
+}
