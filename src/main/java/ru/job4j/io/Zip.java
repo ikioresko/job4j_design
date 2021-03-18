ead754db8b469dc7c7144dcf4c24f6d4ff563e37
ikioresko <ikioresko@gmail.com>
2021-03-18 14:52:12 +0500

    5.2. Архивировать проект [#467344]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -25,30 +25,20 @@ public class Zip {
         }
     }
 
-//    public void packSingleFile(File source, File target) {
-//        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(
-//                new FileOutputStream(target)))) {
-//            zip.putNextEntry(new ZipEntry(source.getPath()));
-//            try (BufferedInputStream out = new BufferedInputStream(
-//            new FileInputStream(source))) {
-//                zip.write(out.readAllBytes());
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
+    private static List<File> getList(Path start, ArgZip argZip) throws IOException {
+        return Search.search(start, p -> !p.toFile()
+                .getName()
+                .endsWith(argZip.exclude()))
+                .stream()
+                .map(Path::toFile)
+                .collect(Collectors.toList());
+    }
 
     public static void main(String[] args) throws IOException {
         ArgZip argZip = new ArgZip(args);
         if (argZip.valid()) {
             Path start = Paths.get(argZip.directory());
-            List<File> files = Search.search(start, p -> !p.toFile()
-                    .getName()
-                    .endsWith(argZip.exclude()))
-                    .stream()
-                    .map(Path::toFile)
-                    .collect(Collectors.toList());
-            new Zip().packFiles(files, new File(argZip.output()));
+            new Zip().packFiles(getList(start, argZip), new File(argZip.output()));
         }
     }
 }
