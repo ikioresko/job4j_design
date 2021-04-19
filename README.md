a25bd839af5f95880cc5e5ce8dd512bd023b5296
Igor Kioresko <ikioresko@gmail.com>
2021-04-19 13:18:17 +0500

    2. Что такое TDD? [#467364]

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 [![Build Status](https://travis-ci.org/ikioresko/job4j_design.svg?branch=master)](https://travis-ci.org/ikioresko/job4j_design)
-[![codecov](https://codecov.io/gh/ikioresko/job4j_design/branch/master/graph/badge.svg?token=FYP0GSXFW0)](https://codecov.io/gh/ikioresko/job4j_design)
+[![codecov](https://codecov.io/gh/ikioresko/job4j_design/branch/master/graph/badge.svg)](https://codecov.io/gh/ikioresko/job4j_design)
---
+++ src/test/java/ru/job4j/tdd/CinemaTest.java
@@ -0,0 +1,68 @@
+package ru.job4j.tdd;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.List;
+
+public class CinemaTest {
+    public Calendar getDate() {
+        Calendar date = Calendar.getInstance();
+        date.set(2020, Calendar.NOVEMBER, 10, 23, 0);
+        return date;
+    }
+
+    @Test
+    public void buy() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Ticket ticket = cinema.buy(account, 1, 1, getDate());
+        assertThat(ticket, is(new Ticket3D()));
+    }
+
+    @Test
+    public void find() {
+        Cinema cinema = new Cinema3D();
+        cinema.add(new Session3D());
+        List<Session> sessions = cinema.find(session -> true);
+        assertThat(sessions, is(Arrays.asList(new Session3D())));
+    }
+
+    @Test(expected = Exception.class)
+    public void whenSessionListIsEmpty() {
+        Cinema cinema = new Cinema3D();
+        cinema.find(session -> true);
+    }
+
+    @Test(expected = Exception.class)
+    public void whenCantFindSession() {
+        Cinema cinema = new Cinema3D();
+        cinema.add(new Session3D());
+        cinema.find(session -> true);
+    }
+
+    @Test(expected = Exception.class)
+    public void whenSessionExistButAllTicketSold() {
+        Cinema cinema = new Cinema3D();
+        cinema.add(new Session3D());
+        cinema.find(session -> true);
+    }
+
+    @Test(expected = Exception.class)
+    public void whenPaymentError() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        cinema.add(new Session3D());
+        cinema.buy(account, 1, 1, getDate());
+    }
+
+    @Test(expected = Exception.class)
+    public void whenSessionAlreadyExist() {
+        Cinema cinema = new Cinema3D();
+        cinema.add(new Session3D());
+    }
+}
