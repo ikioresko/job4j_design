ac4c8b6008eccb1bf79309a81bf44bb07ecad18b
Igor Kioresko <ikioresko@gmail.com>
2021-04-20 17:04:55 +0500

    0. Видео [#467379]

---
+++ src/main/java/ood/ocp/Example.java
@@ -0,0 +1,8 @@
+package ood.ocp;
+
+public class Example extends Printer {
+    public static void main(String[] args) {
+        Example ex1 = new Example();
+        ex1.print("Hello world");
+    }
+}
